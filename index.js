443b0dec1df519b954b876613f6c22dd1814a9ee
PS <PS@HECTOR>
2018-11-16 22:32:50 +0530

    1st

---
+++ index.js
@@ -0,0 +1,5 @@
+require('http').createServer((req, res)=>{
+	let fs = require('fs'), buffer = fs.readFileSync('./WebContent/index.html');
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.end(buffer);
+}).listen(8888);
