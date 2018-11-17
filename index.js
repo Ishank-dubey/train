f192f31cd2e6e4aa17b16870c34b69a2f6eca5ed
PS <PS@HECTOR>
2018-11-18 02:56:12 +0530

    1st

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require('http').createServer((req, res)=>{
-	let fs = require('fs'), buffer = fs.readFileSync('./WebContent/index.html');
-	res.writeHead(200, { 'Content-Type': 'text/html' });
+	let path = '/index.html'
+		if (req.url!='/'){
+			path = 	req.url;
+			if(req.url.match('css'))
+				res.writeHead(200, { 'Content-Type': 'text/css' });
+			else 
+				res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+		}else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+		}
+	let fs = require('fs'), buffer = fs.readFileSync('./WebContent'+path);
 	res.end(buffer);
 }).listen(8888,(port,data)=>{
 	console.log(data);
