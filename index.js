713fdb5b9f546faac7104230159bfb705358723c
PS <PS@HECTOR>
2018-11-17 18:50:14 +0530

    2

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ require('http').createServer((req, res)=>{
 	let fs = require('fs'), buffer = fs.readFileSync('./WebContent/index.html');
 	res.writeHead(200, { 'Content-Type': 'text/html' });
 	res.end(buffer);
-}).listen(8888);
+}).listen(8888,(port,data)=>{
+	console.log(data);
+});
